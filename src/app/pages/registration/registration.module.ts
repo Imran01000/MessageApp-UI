7cf33da01d178b810d542abfa7214a2900af8118
Imran01000 <imshaikh01000@gmail.com>
2020-10-11 21:14:12 +0530

    ADDED:Added module file

---
+++ src/app/pages/registration/registration.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegistrationPageRoutingModule } from './registration-routing.module';
+
+import { RegistrationPage } from './registration.page';
+
+import { ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RegistrationPageRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [RegistrationPage]
+})
+export class RegistrationPageModule {}
